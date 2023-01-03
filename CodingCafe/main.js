384f3d3345becfe0f6c32ac9d20f54652898146d
nanopapa <88864315+nanopapa@users.noreply.github.com>
2023-01-04 08:13:14 +0900

    Add files via upload

---
+++ CodingCafe/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+$(function(){
+    $('a[href^="#"]').click(function(){
+    let href= $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, 600, "swing");
+    return false;
+    });
+})
